450c3ae468624d1b910a7ad29552ad83fabb9b9a
chrisdedmonds <chris.edmonds128@gmail.com>
2015-03-27 20:39:42 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Certification.delete_all
+
+Certification.create!(cert_name:'Microsoft Certified IT Professional', cert_company: 'Microsoft', category: 'Information Technology', description: 'This certification covers a broad spectrum of information technology skills and capabilities. MCITPs are strong performers ready to enter a wide variety of technology positions.', logo_url: 'http://www.geekwithenvy.com/wp-content/uploads/2012/10/MSFT_logo_Small1.jpg')
+Certification.create!(cert_name:'Microsoft Certified Solutions Developer', cert_company: 'Microsoft', category: 'Software Development', description: 'This certification covers a broad spectrum of information technology skills and capabilities. MCSDs are strong performers ready to enter a wide variety of technology positions.', logo_url: 'http://www.geekwithenvy.com/wp-content/uploads/2012/10/MSFT_logo_Small1.jpg')
+Certification.create!(cert_name:'Private Cloud Certification', cert_company: 'Microsoft', category: 'Cloud', description: 'This certification covers a broad spectrum of information technology skills and capabilities. PCCs are strong performers ready to enter a wide variety of technology positions.', logo_url: 'http://www.geekwithenvy.com/wp-content/uploads/2012/10/MSFT_logo_Small1.jpg')
