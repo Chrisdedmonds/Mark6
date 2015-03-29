581932ff97e01a5701e6ba2f1757a2637d5dfe93
chrisdedmonds <chris.edmonds128@gmail.com>
2015-03-29 17:13:06 -0400

    first certification unit test

--- test/models/certification_test.rb
+++ test/models/certification_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CertificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "certification attributes must not be empty" do
+  	certification = Certification.new
+  	assert certification.invalid?
+  	assert certification.errors[:cert_name].any?
+  	assert certification.errors[:cert_company].any?
+  	assert certification.errors[:category].any?
+  	assert certification.errors[:description].any?
+  	assert certification.errors[:logo_url].any?
+  end
+ end
+
